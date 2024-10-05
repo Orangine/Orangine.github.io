1e7ca3e161bd73fa26c48637c58660a6ee937a7b
OrangineOwO <166426720+Pixel-Memories@users.noreply.github.com>
2024-10-05 19:00:20 -0300

    Update images.js

--- images.js
+++ images.js
@@ -21,5 +21,6 @@ const images=[
     {src:"https://i.imgur.com/eJPJHRK.jpeg",thumb:"https://i.imgur.com/NW4oWxu.jpeg",title:"Borderlands 2",author:"Orangine", date:"02-10-2024", type:"retrato", wpp:"sim"},
     {src:"https://i.imgur.com/g3LQ7AD.jpeg",thumb:"https://i.imgur.com/vGrx5Ou.jpeg",title:"Star Wars Jedi: Survivor",author:"Orangine", date:"18-09-2024", type:"paisagem", wpp:"sim"},
     {src:"https://i.imgur.com/HaXolhy.jpeg",thumb:"https://i.imgur.com/VF4eHM1.jpeg",title:"Baldur's Gate 3",author:"Orangine", date:"25-08-2024", type:"paisagem", wpp:"sim"},
-    {src:"https://i.imgur.com/4vyIsv2.jpeg",thumb:"https://i.imgur.com/VBOMnxx.jpeg",title:"Star Wars Jedi: Survivor",author:"Orangine", date:"16-09-2024", type:"paisagem", wpp:"sim"}
+    {src:"https://i.imgur.com/4vyIsv2.jpeg",thumb:"https://i.imgur.com/VBOMnxx.jpeg",title:"Star Wars Jedi: Survivor",author:"Orangine", date:"16-09-2024", type:"paisagem", wpp:"sim"},
+    {src:"https://i.imgur.com/48qedxX.jpeg",thumb:"https://i.imgur.com/zqIyAjR.jpeg",title:"Horizon Zero Dawn",author:"Jujuba", date:"13-09-2024", type:"paisagem", wpp:"sim"}
 ];
